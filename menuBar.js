f8046b7287c5f182d68aa08a1f4b9e7c83d4b7dc
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-05-19 16:03:15 +0630

    Update menu bar by linking with each section

--- menuBar.js
+++ menuBar.js
@@ -1,6 +1,7 @@
 const openMenuBar = document.getElementById('open-btn');
 const navWrapper = document.getElementById('nav-wrapper');
 const crossBtn = document.getElementById('cross-btn');
+const navLinks = document.querySelectorAll('.nav-items li');
 
 openMenuBar.addEventListener('click', () => {
   navWrapper.classList.toggle('active');
@@ -10,4 +11,11 @@ openMenuBar.addEventListener('click', () => {
 crossBtn.addEventListener('click', () => {
   navWrapper.classList.toggle('active');
   navWrapper.classList.toggle('open');
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    navWrapper.classList.toggle('active');
+    navWrapper.classList.toggle('open');
+  });
 });
